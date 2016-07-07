38471065f0ae99b62328c1ca0a8b981a38f9d596
ruqli <ruqli@microsoft.com>
2016-07-07 14:02:59 +0800

    Add iOS libraries

--- Category/JavaScript/List-of-languages-that-compile-to-JS.sh
+++ Category/JavaScript/List-of-languages-that-compile-to-JS.sh
@@ -1,5 +1,4 @@
 pandoc -f markdown_github -t mediawiki -o l.mediawiki List-of-languages-that-compile-to-JS.md
-
-cat l.mediawiki | awk -F"|" '{if (substr($1,0,6) ==  "[[File") print substr($1,8) "&.png"; else print $0; }' > List-of-languages-that-compile-to-JS.mediawiki
-
-rm l.mediawiki
+cat l.mediawiki | awk -F"|" '{if (substr($1,0,6) ==  "[[File") print substr($1,8); else print $0; }' > List-of-languages-that-compile-to-JS.mediawiki
+sed -i .back -e 's/\.svg\?style=flat-square/\.png/g' List-of-languages-that-compile-to-JS.mediawiki
+rm l.mediawiki l1.mediawiki.back
